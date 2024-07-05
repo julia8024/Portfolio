0da3bd320367f7431f2bb713dd712e256becaf82
julia8024 <julia8024@naver.com>
2024-07-06 00:26:27 +0900

    Feat: change navigation bar code

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,5 @@
-import Navigation from "@/components/navigation"
-
 export default function About() {
     return (<>
-        <Navigation />
         <div>about!</div>
     </>
     );
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,5 @@
-import Navigation from "@/components/navigation";
-
 export default function NotFound() {
     return (<>
-        <Navigation />
         <div>Not Found</div>
         </>
     );
